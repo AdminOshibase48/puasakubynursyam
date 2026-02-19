f4aeffa2b1cc4393e81d2cca861a697e958a3d39
AdminOshibase48 <oshibase48.ofc@gmail.com>
2026-02-19 18:02:51 +0700

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'puasaku-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/login.html',
+    '/dashboard.html',
+    '/css/style.css',
+    '/js/auth.js',
+    '/js/dashboard.js',
+    '/js/utils.js',
+    '/manifest.json'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Activate service worker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event handler
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Return cached version or fetch new
+                return response || fetch(event.request);
+            })
+    );
+});
